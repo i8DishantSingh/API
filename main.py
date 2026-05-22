b3c4bb486ecdcfdfe3976061becbee8da1b9592c
i8DishantSingh <professional.id.dishant@gmail.com>
2026-05-22 12:37:10 +0530

    minor adjustments

--- main.py
+++ main.py
@@ -4,6 +4,6 @@ from routes.orderRoutes import order_router
 
 
 app = FastAPI()
-app.include_router(auth_router)
-app.include_router(order_router)
+app.include_router(auth_router, prefix="/api/v1")
+app.include_router(order_router, prefix="/api/v1")
 
