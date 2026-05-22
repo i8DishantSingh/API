f7204babebe6887e7deef479418b3b34cff43284
i8DishantSingh <professional.id.dishant@gmail.com>
2026-05-22 12:36:23 +0530

    minor tweaks in routes

--- routes/authRoutes.py
+++ routes/authRoutes.py
@@ -1,10 +1,8 @@
 from fastapi import APIRouter
 
-auth_router = APIRouter(
-    prefix="/auth"
-)
+auth_router = APIRouter(prefix="/auth", tags=["auth"])
 
-@auth_router.get("/")
+@auth_router.get("/signup")
 async def get_auth():
     return {"message": "Hello world"}
 
--- routes/orderRoutes.py
+++ routes/orderRoutes.py
@@ -1,4 +1,8 @@
 from fastapi import APIRouter
 
 
-order_router = APIRouter()
+order_router = APIRouter(prefix="/order", tags=['order'])
+
+@order_router.get("/getorders")
+def get_orders():
+    return {"messages" : "No Orders"}
